5292fd17514d87d93d46fba2d7acd4c6db685170
SeanSassenrath <sean.sassenrath@gmail.com>
2015-06-18 18:02:20 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  $('#invalid-request').hide();
+  $('#ajax-error').hide();
+
+  $('#contact-submit').on('click', function(e){
+    e.preventDefault();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var message = $('#message').val();
+
+    if (name.length > 0 && email.length > 0) {
+      $.ajax({
+        url: 'http://getsimpleform.com/messages/ajax?form_api_token=11545f80afa85c2500d908afd880b345',
+        type: 'POST',
+        dataType: 'jsonp',
+        data: {name: name, email: email, subject: subject, message: message}
+      }).done(function() {
+        $('#name').hide();
+        $('#email').hide();
+        $('#subject').hide();
+        $('#message').hide();
+        $('#invalid-request').hide();
+        $('#ajax-error').hide();
+        $('#contact-submit').val('Thanks for your message!');
+        $('#contact-submit').css("color", "#66CDAA");
+        $('#contact-submit').css("border-color", "#66CDAA");
+        $('form')[0].reset();
+      }).fail(function(){
+        $('#ajax-error').show();
+      })
+    } else {
+      $('#invalid-request').show();
+    };
+
+  });
+});
