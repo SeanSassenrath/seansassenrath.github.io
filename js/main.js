e3a93d9088863938a738224b3c28a1554f19920d
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-07-24 00:59:49 -0700

    Don't activate current project prompt on smaller screens

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 
 (function() {
 
+  // Days worked at employer banner
   const today = new Date();
   const dates = { msftStartDate: new Date(2016, 6, [1]) }
 
   const daysWorkedBanner = Math.floor((today - dates.msftStartDate) / (1000 * 60 * 60 * 24));
   document.getElementById('days').innerHTML = daysWorkedBanner;
 
+  // Current project prompt
   const currentProjectLink = document.getElementById('current-project-link');
   const currentProjectPrompt = document.getElementById('current-project-prompt');
 
-  currentProjectLink.addEventListener('mouseover', function(e) {
+  const showCurrentProjectPrompt = function(e) {
     currentProjectPrompt.className = 'prompt-display--active prompt'
-  })
-  currentProjectLink.addEventListener('mouseout', function(e) {
+  };
+  const hideCurrentProjectPrompt = function(e) {
     currentProjectPrompt.className = 'prompt-display--hidden prompt'
-  })
+  };
 
+  const currentProjectPromptCheck = function() {
+    // Only show current project prompt if in a larger window
+    if (window.innerWidth > 700) {
+      currentProjectLink.addEventListener('mouseover', showCurrentProjectPrompt)
+      currentProjectLink.addEventListener('mouseout', hideCurrentProjectPrompt)
+    } else {
+      currentProjectLink.removeEventListener('mouseover', showCurrentProjectPrompt)
+      currentProjectLink.removeEventListener('mouseout', hideCurrentProjectPrompt)
+    }
+  }
+  // Check if project prompt should be active on load;
+  currentProjectPromptCheck();
+  // Check if project prompt should be active on window resizing
+  window.addEventListener('resize', currentProjectPromptCheck);
 })();
